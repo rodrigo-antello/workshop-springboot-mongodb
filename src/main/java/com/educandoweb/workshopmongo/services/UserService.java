073212f0017686fa5647180e086a98778d84cd9a
Rodrigo Antello <rodrigo.antello@outlook.com>
2022-07-05 22:22:52 -0300

    Getting a user by id

--- src/main/java/com/educandoweb/workshopmongo/services/UserService.java
+++ src/main/java/com/educandoweb/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.educandoweb.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.workshopmongo.domain.User;
 import com.educandoweb.workshopmongo.repositories.UserRepository;
+import com.educandoweb.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
