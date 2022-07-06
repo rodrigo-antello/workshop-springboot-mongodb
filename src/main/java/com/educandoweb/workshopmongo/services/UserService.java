61fc1e73d06825278cb51c2b4834d58f00b794cb
Rodrigo Antello <rodrigo.antello@outlook.com>
2022-07-05 23:23:03 -0300

    User update with PUT

--- src/main/java/com/educandoweb/workshopmongo/services/UserService.java
+++ src/main/java/com/educandoweb/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.educandoweb.workshopmongo.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,21 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		try {
+			User newObj = repository.findById(obj.getId()).get();
+			updateData(newObj, obj);
+			return repository.save(newObj);
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("Object not found");
+		}
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
